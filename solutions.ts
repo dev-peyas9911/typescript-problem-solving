3a0d2fb224d4a5f4c52ae9117362e6220a924470
Peyas Barmon <dev.peyasbarmon0024@gmail.com>
2026-05-07 18:29:19 +0600

    problem 3 solved

--- solutions.ts
+++ solutions.ts
@@ -6,11 +6,27 @@ function filterEvenNumbers(numbers: number[]): number[] {
 
 console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
 
-
 // Problem 2
 
 function reverseString(str: string): string {
-    return str.split('').reverse().join('');
+  return str.split("").reverse().join("");
 }
 
 console.log(reverseString("typescript"));
+
+// Problem 3
+
+type StringOrNumber = string | number;
+
+function checkType(value: StringOrNumber): "String" | "Number" {
+  if (typeof value === "string") {
+    return "String";
+  } else {
+    return "Number";
+  }
+}
+
+console.log(checkType("Hello"));
+console.log(checkType(25));
+
+
